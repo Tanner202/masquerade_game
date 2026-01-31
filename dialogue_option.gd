87ec0025749165551bc71925b185be57e273e08b
Tanner King <96085009+Tanner202@users.noreply.github.com>
2026-01-31 12:00:55 -0500

    add basic dialogue system

--- dialogue_option.gd
+++ dialogue_option.gd
@@ -1,10 +1,12 @@
 class_name DialogueOption extends Button
 
-@export var next_dialogue: PackedScene
+@export var dialogue_owner: NPC
+@export var next_dialogue = ""
 @export var suspicion_impact := 0
 
 func _on_pressed() -> void:
 	# TODO: Impact suspicion
+	dialogue_owner.move_dialogue(next_dialogue)
 	print("selected dialogue")
 	#var next_dialogue_instance = next_dialogue.instantiate()
 	#get_tree().root.add_child(next_dialogue_instance)
