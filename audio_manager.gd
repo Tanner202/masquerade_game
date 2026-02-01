978a396ecef028698cc06ef58426857f8c49b413
Tanner King <96085009+Tanner202@users.noreply.github.com>
2026-02-01 14:34:43 -0500

    add music

--- audio_manager.gd
+++ audio_manager.gd
@@ -1,9 +1,22 @@
 class_name AudioManager extends Node
 
-static func play_sound(sfx: AudioStream, owner: Node2D):
+const MAINTHEME = preload("res://Audio/maintheme.wav")
+
+func _ready() -> void:
+	play_music(MAINTHEME)
+
+func play_sound(sfx: AudioStream, owner: Node2D):
 	var audioPlayer = AudioStreamPlayer2D.new()
+	get_tree().root.add_child(audioPlayer)
 	audioPlayer.global_position = owner.global_position
 	audioPlayer.stream = sfx
 	audioPlayer.play()
 	await audioPlayer.finished
 	audioPlayer.queue_free()
+
+func play_music(music: AudioStream):
+	var musicPlayer = AudioStreamPlayer.new()
+	add_child(musicPlayer)
+	musicPlayer.stream = music
+	musicPlayer.play()
+	
