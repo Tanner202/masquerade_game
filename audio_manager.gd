db5a87e4b53e608365474dea9de558432b1296ee
Tanner King <96085009+Tanner202@users.noreply.github.com>
2026-02-01 16:56:26 -0500

    fix music

--- audio_manager.gd
+++ audio_manager.gd
@@ -4,7 +4,8 @@ const MAIN_THEME = preload("res://Audio/maintheme.wav")
 const BALLROOM = preload("res://Audio/ballroom.wav")
 
 func _ready() -> void:
-	play_music(MAIN_THEME)
+	if get_tree().current_scene.name == "Level":
+		play_music(MAIN_THEME)
 	get_tree().scene_changed.connect(_on_scene_changed)
 
 func play_sound(sfx: AudioStream, owner: Node2D):
@@ -24,7 +25,7 @@ func play_music(music: AudioStream):
 	
 func _on_scene_changed():
 	var scene = get_tree().current_scene
-	if scene.name == "ballroom":
+	if scene.name == "Ballroom":
 		play_music(BALLROOM)
 	else:
 		play_music(MAIN_THEME)
