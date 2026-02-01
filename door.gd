c96943d4b2217bf112932268ba8af1b87a867f0b
Tanner King <96085009+Tanner202@users.noreply.github.com>
2026-02-01 16:30:45 -0500

    add room transition manager

--- door.gd
+++ door.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 var player_in_interaction_range = false
 var player: CharacterBody2D = null
-@export var room_scene: PackedScene
+@export var room_path: String
+@export var target_door: String
 @onready var interaction_prompt = $CanvasLayer/InteractionPrompt
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
@@ -19,4 +20,5 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact") and player_in_interaction_range:
-		get_tree().change_scene_to_packed(room_scene)
+		TransitionManager.set_target_door(target_door)
+		TransitionManager.change_scene(room_path)
